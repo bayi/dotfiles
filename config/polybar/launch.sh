ae31988df681f0bde38f025cdb2dd328e2a70356
Bayi <bayi@bayi.hu>
2019-11-21 23:48:46 +0100

    Fix bar

---
+++ config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep .5; done
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        t=none
+        if [ "$m" == "HDMI2" ]; then
+            t=right
+        fi
+        MONITOR=$m TRAY=$t polybar --reload top &
+    done
+else
+    polybar --reload top &
+fi
