92be0fe3c1ccbb86b824f8abfaab422d59781f7a
Bayi <bayi@bayi.hu>
2019-11-22 01:33:43 +0100

    Fix laptop screen

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,7 +6,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep .5; done
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         t=none
-        if [ "$m" == "HDMI2" ]; then
+        if [ "$m" == "HDMI2" ] || [ "$m" == "LVDS1" ]; then
             t=right
         fi
         MONITOR=$m TRAY=$t polybar --reload top &
